03302b2a600e7e6cbbcf3da9a998cac780123363
mohamadnaufalazim.mf <mohamadnaufalazim.mf@petronas.com>
2023-11-06 21:30:32 +0800

    LinkedList Constructor

---
+++ LinkedList/Constructor/Main.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList myLinkedList = new LinkedList(4);
+
+        // myLinkedList.getHead();
+        // myLinkedList.getTail();
+        // myLinkedList.getLength();
+
+        // System.out.println("\nLinked List:");
+        // myLinkedList.printList();
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Head: 4
+            Tail: 4
+            Length: 1
+            
+            Linked List:
+            4
+
+        */
+
+    }
+
+}
